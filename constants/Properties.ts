5974a590621a669b3876b4974a716a59fe76b7d0
mayank2130 <mayankthakur1712@gmail.com>
2024-03-04 00:59:09 +0530

    commercial properties

---
+++ constants/Properties.ts
@@ -0,0 +1,87 @@
+export interface Property {
+  id: string;
+  price: string;
+  location: string;
+  source: number; // Assuming 'source' is of type number (require statement)
+}
+
+export const propertyData: Property[] = [
+  {
+    id: "1",
+    price: "28,000,000",
+    location: "Alpine, USA",
+    source: require("../assets/buildings/alpine.png"),
+  },
+  {
+    id: "2",
+    price: "28,000,000",
+    location: "California, USA",
+    source: require("../assets/buildings/california.jpg"),
+  },
+  {
+    id: "3",
+    price: "28,000,000",
+    location: "Cape Town, South Africa",
+    source: require("../assets/buildings/capetown.jpg"),
+  },
+  {
+    id: "4",
+    price: "28,000,000",
+    location: "Chicago, USA",
+    source: require("../assets/buildings/chicago.jpg"),
+  },
+  {
+    id: "5",
+    price: "28,000,000",
+    location: "Chicago, USA",
+    source: require("../assets/buildings/chicago2.jpg"),
+  },
+  {
+    id: "6",
+    price: "28,000,000",
+    location: "Chicago, USA",
+    source: require("../assets/buildings/chicago3.jpg"),
+  },
+  {
+    id: "7",
+    price: "28,000,000",
+    location: "Cyprus",
+    source: require("../assets/buildings/cyprus.jpg"),
+  },
+  {
+    id: "8",
+    price: "28,000,000",
+    location: "Dubai, UAE",
+    source: require("../assets/buildings/dubai.jpg"),
+  },
+  {
+    id: "9",
+    price: "28,000,000",
+    location: "Hong Kong",
+    source: require("../assets/buildings/hongkong.jpg"),
+  },
+  {
+    id: "10",
+    price: "28,000,000",
+    location: "London, UK",
+    source: require("../assets/buildings/london.jpg"),
+  },
+  {
+    id: "11",
+    price: "28,000,000",
+    location: "London, UK",
+    source: require("../assets/buildings/londontwo.jpg"),
+  },
+  {
+    id: "12",
+    price: "28,000,000",
+    location: "Malaysia",
+    source: require("../assets/buildings/malasiya.jpg"),
+  },
+  {
+    id: "13",
+    price: "28,000,000",
+    location: "Shenzhen, China",
+    source: require("../assets/buildings/shenzhen.jpg"),
+  },
+];
